00fe2ba8d20a7f54f8cbf0e621a1f41f23633f73
bochatayy <yannick.bochatay@meteo.fr>
2016-03-14 16:55:08 +0100

    bug with amd

--- JSYG.Editor.js
+++ JSYG.Editor.js
@@ -3,7 +3,7 @@
 
 (function(factory) {
 
-    if (typeof define != "undefined" && define.amd) define("jsyg-editor",["jsyg","jsyg-path","jsyg-boudingbox","jsyg-selection","jsyg-container","jsyg-rotatable","jsyg-draggable","jsyg-resizable","jsyg-alignment"],factory);
+    if (typeof define != "undefined" && define.amd) define("jsyg-editor",["jsyg","jsyg-path","jsyg-boundingbox","jsyg-selection","jsyg-container","jsyg-rotatable","jsyg-draggable","jsyg-resizable","jsyg-alignment"],factory);
     else if (typeof JSYG != "undefined") {
         if (JSYG.Path && JSYG.Vect && JSYG.BoundingBox && JSYG.Selection && JSYG.Container && JSYG.Draggable && JSYG.Resizable && JSYG.Rotatable && JSYG.Alignment) factory(JSYG,JSYG.Path,JSYG.BoundingBox,JSYG.Selection,JSYG.Container,JSYG.Rotatable);
         else throw new Error("Dependency is missing");
